c1f2a7b0984208893ab9a214c958251f1f24e218
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-08-13 22:38:00 +0500

    Create_DZ_mashine_state

---
+++ module_13_4.py
@@ -0,0 +1,51 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = '7393565379:AAGYjiLA29gIeez1X0u74uZLwhlCOv1Wv6E'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Привет')
+async def hello_message(message):
+    await message.answer('Введите команду /start, чтобы начать диалог')
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет, я Бот, помогающий твоему здоровью. Введите слово Calories.')
+@dp.message_handler(text='Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст.')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(first=message.text)
+    await message.answer('Введите свой рост.')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(second=message.text)
+    await message.answer('Введите свой вес.')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(third=message.text)
+    data = await state.get_data()
+    calc_calories = (10 * int(data['third'])) + (6.25 * int(data['second'])) - (5 * int(data['first'])) + 5
+    await message.answer(f'Ваша суточная норма каллорий составляет: {calc_calories}')
+    await state.finish()
+    print(type(data['second']))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
